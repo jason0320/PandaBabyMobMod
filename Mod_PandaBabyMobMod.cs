419747704d0cc8f0705badf21095ef62c0281ce4
jason0320 <jasonnosaj0320@gmail.com>
2025-04-23 21:44:20 +0800

    加入專案檔案。

---
+++ Mod_PandaBabyMobMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+
+namespace Mod_PandaBabyMobMod
+{
+    [BepInPlugin("panda.babymob.mod", "Panda's Baby Mob Mod", "1.0.0.0")]
+    internal class Mod_PandaBabyMobMod : BaseUnityPlugin
+    {
+        private void OnStartCore()
+        {
+            string pathToExcelFile = Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location) + "/BabyMob.xlsx";
+            SourceManager sources = Core.Instance.sources;
+            ModUtil.ImportExcel(pathToExcelFile, "Chara", sources.charas);
+        }
+    }
+}
